531d70d66bd05d45f89e285e2129de1fd9fbc7b8
ChiZhangCZ <chi.zhang@academytrainee.com>
2018-08-30 13:55:32 +0100

    DVD commit

---
+++ ChiVDs/src/test/java/com/chi/zhang/springboot/ChiVD/ChiVDsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chi.zhang.springboot.ChiVD;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChiVDsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
